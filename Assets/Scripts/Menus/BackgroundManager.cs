2696cb776ae48c8b43e22b00330aa3a6af9b3313
Tom Kauffeld <tom@kauffeld-it.fr>
2023-07-08 18:20:07 +0200

    start of background manager

---
+++ Assets/Scripts/Menus/BackgroundManager.cs
@@ -0,0 +1,12 @@
+using Assets.Scripts.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MyMonoBehaviour
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
